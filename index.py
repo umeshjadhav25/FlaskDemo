2329d336c3f22217b3c93b6609c9d43d32d02cdb
Umesh Jadhav <ujadhav2@in.ibm.com>
2019-12-27 17:34:34 +0530

    Added templates functionality

--- index.py
+++ index.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Welcome Umesh."
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', name = "Umesh")
 
 app.run()
